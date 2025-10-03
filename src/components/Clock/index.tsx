8779f1af041a3724e8e049bd3a371de3fd56f6de
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-03 17:14:31 +0200

    Implement Clock component

---
+++ src/components/Clock/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function Clock() {
+  const [time, setTime] = useState<Date>(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <p className="text-white text-sm">{time.toLocaleTimeString("en-US")}</p>
+  );
+}
+
+export default Clock;
