81155a6b89fed635bd2f4aa36f5f0fd884e65314
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-07 11:43:30 +0200

    Add context menu support

--- src/components/Desktop/index.tsx
+++ src/components/Desktop/index.tsx
@@ -1,9 +1,22 @@
+import type { MouseEvent } from "react";
 import Panel from "../Panel";
+import { useContextMenuStore } from "../../store/contextMenuStore";
+import ContextMenu from "../ContextMenu";
 
 function Desktop() {
+  const setIsOpen = useContextMenuStore((state) => state.setIsOpen);
+  const setPosition = useContextMenuStore((state) => state.setPosition);
+
+  const handleContextMenu = (event: MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setPosition(event.pageX, event.pageY);
+    setIsOpen(true);
+  };
+
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full" onContextMenu={handleContextMenu}>
       <Panel />
+      <ContextMenu />
     </div>
   );
 }
