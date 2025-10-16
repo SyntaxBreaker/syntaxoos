4ea483ef4665f0c191eabcc5016423f73d42be91
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-16 13:34:21 +0200

    Improve icon size in DesktopIcon

--- src/components/DesktopIcon/index.tsx
+++ src/components/DesktopIcon/index.tsx
@@ -10,7 +10,7 @@ function DesktopIcon({ name, iconSrc, onClick }: DesktopIconProps) {
       className="flex flex-col gap-2 w-16 flex-wrap cursor-pointer"
       onClick={onClick}
     >
-      <img src={iconSrc} alt={name} />
+      <img src={iconSrc} alt={name} className="h-6 w-6 m-auto" />
       <p className="text-white text-center">{name}</p>
     </div>
   );
