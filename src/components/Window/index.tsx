287b466737941e59d7dca00c094cf27b00e6f77c
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-17 13:51:33 +0200

    Implement Window component

---
+++ src/components/Window/index.tsx
@@ -0,0 +1,26 @@
+interface WindowProps {
+  x: number;
+  y: number;
+  children: React.ReactNode;
+}
+
+function Window({ x, y, children }: WindowProps) {
+  return (
+    <div
+      className="flex flex-col gap-4 h-[640px] w-[640px] p-2 bg-panel absolute rounded-sm"
+      style={{left: `${x}px`, top: `${y}px`}}
+    >
+      <div className="flex flex-row justify-between items-center">
+        <p className="text-white text-sm font-bold">Title</p>
+        <div className="flex flex-row gap-1">
+          <div className="h-2 w-2 bg-green-600 rounded-xs"></div>
+          <div className="h-2 w-2 bg-yellow-600 rounded-xs"></div>
+          <div className="h-2 w-2 bg-red-600 rounded-xs"></div>
+        </div>
+      </div>
+      <div className="h-full w-full">{children}</div>
+    </div>
+  );
+}
+
+export default Window;
