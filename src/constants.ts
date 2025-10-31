4e91e5af978330288ba0a3939622245862c6d184
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-31 12:22:21 +0100

    Implement app constants

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+import folderIcon from "./assets/folder.svg";
+import terminalIcon from "./assets/folder.svg";
+
+interface App {
+  id: number;
+  name: string;
+  iconSrc: string;
+}
+
+export const APPS: App[] = [
+  {
+    id: 0,
+    name: "File manager",
+    iconSrc: folderIcon,
+  },
+  {
+    id: 1,
+    name: "Terminal",
+    iconSrc: terminalIcon,
+  },
+];
