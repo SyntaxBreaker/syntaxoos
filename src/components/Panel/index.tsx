600230d0015fa4ada6e25067349d636874666fac
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-31 12:24:44 +0100

    Implement active windows in Panel

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,12 +1,32 @@
 import menuIcon from "../../assets/menuicon.svg";
+import { useWindowsStore } from "../../store/windowsStore";
 import Clock from "../Clock";
+import { APPS } from "../../constants";
 
 function Panel() {
+  const activeWindows = useWindowsStore((state) => state.activeWindows)
+    .map((activeWindow) => APPS.find((app) => app.id === activeWindow.id))
+    .filter((app) => app !== undefined);
+
   return (
     <div className="fixed bottom-0 p-2 bg-panel w-full flex flex-row justify-between">
-      <button className="p-0 hover:cursor-pointer" aria-label="Open menu">
-        <img src={menuIcon} alt="menu icon" />
-      </button>
+      <div className="flex flex-row gap-4">
+        <button className="p-0 hover:cursor-pointer" aria-label="Open menu">
+          <img src={menuIcon} alt="menu icon" />
+        </button>
+        <div className="flex flex-row gap-4">
+          {activeWindows.map((activeWindow) => (
+            <div key={activeWindow.id} className="flex flex-row gap-1">
+              <img
+                src={activeWindow.iconSrc}
+                alt={activeWindow.name}
+                className="h-4 w-4 self-center"
+              />
+              <p className="text-white text-sm">{activeWindow.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
       <Clock />
     </div>
   );
