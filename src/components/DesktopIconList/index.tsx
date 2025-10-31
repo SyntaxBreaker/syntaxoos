f8ae9c15cd5fe547328e6d5180dbbe05a7431b9e
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-10-31 12:22:51 +0100

    Refactor DesktopIconList component

--- src/components/DesktopIconList/index.tsx
+++ src/components/DesktopIconList/index.tsx
@@ -1,26 +1,6 @@
 import DesktopIcon from "../DesktopIcon";
-import folderIcon from "../../assets/folder.svg";
-import terminalIcon from "../../assets/terminal.svg";
 import { useWindowsStore } from "../../store/windowsStore";
-
-interface DesktopIconItem {
-  id: number;
-  name: string;
-  iconSrc: string;
-}
-
-const DESKTOP_ICONS: DesktopIconItem[] = [
-  {
-    id: 0,
-    name: "File manager",
-    iconSrc: folderIcon,
-  },
-  {
-    id: 1,
-    name: "Terminal",
-    iconSrc: terminalIcon,
-  },
-];
+import { APPS } from "../../constants";
 
 function DesktopIconList() {
   const openWindow = useWindowsStore((state) => state.openWindow);
@@ -31,12 +11,12 @@ function DesktopIconList() {
 
   return (
     <div className="flex flex-col gap-4 h-[calc(100%-36px)] w-full p-4">
-      {DESKTOP_ICONS.map((desktopIcon) => (
+      {APPS.map((app) => (
         <DesktopIcon
-          key={desktopIcon.id}
-          name={desktopIcon.name}
-          iconSrc={desktopIcon.iconSrc}
-          onClick={() => handleIconClick(desktopIcon.id)}
+          key={app.id}
+          name={app.name}
+          iconSrc={app.iconSrc}
+          onClick={() => handleIconClick(app.id)}
         />
       ))}
     </div>
