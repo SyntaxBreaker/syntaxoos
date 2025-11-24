0fd0e4bb0237521cf471f681b37b1c91abf95c60
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-11-24 15:34:29 +0100

    Implement FileManagerContextMenu

---
+++ src/components/FileManagerContextMenu/index.tsx
@@ -0,0 +1,30 @@
+import { useContextMenuStore } from "../../store/contextMenuStore";
+import ContextMenu from "../ContextMenu";
+import ContextMenuItem from "../ContextMenuItem";
+
+function FileManagerContextMenu() {
+  const activeId = useContextMenuStore((state) => state.activeId);
+  const x = useContextMenuStore((state) => state.x);
+  const y = useContextMenuStore((state) => state.y);
+  const closeContextMenu = useContextMenuStore((state) => state.closeContextMenu);
+  const isOpen = activeId === "fileManager";
+
+  return (
+    <ContextMenu
+      isOpen={isOpen}
+      position={{ x: x, y: y }}
+      onClose={() => closeContextMenu()}
+    >
+      <ContextMenuItem
+        label="Refresh"
+        onClick={() => window.location.reload()}
+      />
+      <ContextMenuItem
+        label="Create a new folder"
+        onClick={() => console.log(`xD`)}
+      />
+    </ContextMenu>
+  );
+}
+
+export default FileManagerContextMenu;
