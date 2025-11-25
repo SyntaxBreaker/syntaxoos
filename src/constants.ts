ba4796f6ad09b814d3f67a4e329d2fa83fa87653
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2025-11-25 15:48:46 +0100

    Add INITIAL_FILE_LIST to constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 import folderIcon from "./assets/folder.svg";
 import terminalIcon from "./assets/terminal.svg";
+import type { FileList } from "./types";
 
 interface App {
   id: number;
@@ -35,4 +36,43 @@ export const commandHandlers = {
   help: () => [`Available commands: ${COMMANDS.join(", ")}`],
   about: () => SYSTEM_INFO,
   echo: (input: string) => input.slice(5),
+};
+
+export const INITIAL_FILE_LIST: FileList = {
+  home: [
+    {
+      id: 0,
+      name: "password.txt",
+    },
+    {
+      id: 1,
+      name: "TODO.md",
+    },
+  ],
+  downloads: [
+    {
+      id: 2,
+      name: "install_me_totally_safe.sh",
+    },
+    {
+      id: 3,
+      name: "memes_backup.tar.gz",
+    },
+  ],
+  music: [
+    {
+      id: 4,
+      name: "random_noises_at_3am.wav",
+    },
+  ],
+  pictures: [
+    {
+      id: 5,
+      name: "never_send_anyone.png",
+    },
+    {
+      id: 6,
+      name: "meme_that_made_me_laugh.png",
+    },
+  ],
 };
