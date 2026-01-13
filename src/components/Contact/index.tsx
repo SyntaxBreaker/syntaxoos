f318da921683434de43e3d72509e1e518dee1f9b
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-01-13 15:57:19 +0100

    Fix contact form submission

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <iframe
       src="https://syntaxbreaker.netlify.app/contact/"
-      sandbox="allow-scripts allow-forms allow-popups"
+      sandbox="allow-scripts allow-forms allow-popups allow-same-origin allow-top-navigation-by-user-activation"
       className="h-full w-full"
     ></iframe>
   );
