46b6c83c0eee49ccf51e8d1df78ba6460e1ba6e1
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-02-02 17:45:53 +0100

    Implement PanelTaskItem component

---
+++ src/components/PanelTaskItem/index.tsx
@@ -0,0 +1,28 @@
+import { useWindowsStore } from "../../store/windowsStore";
+import type { App } from "../../types";
+
+interface PanelTaskItemProps {
+  activeWindow: App;
+}
+
+function PanelTaskItem({ activeWindow }: PanelTaskItemProps) {
+  const toggleMinimize = useWindowsStore((state) => state.toggleMinimize);
+
+  return (
+    <button
+      key={activeWindow.id}
+      className="flex flex-row gap-2 items-center cursor-pointer"
+      onClick={() => toggleMinimize(activeWindow.id)}
+      aria-label={`Toggle ${activeWindow.windowName}`}
+    >
+      <img
+        src={activeWindow.iconSrc}
+        alt={activeWindow.windowName}
+        className="h-4 w-4 self-center"
+      />
+      <p className="text-white text-sm">{activeWindow.windowName}</p>
+    </button>
+  );
+}
+
+export default PanelTaskItem;
--- src/components/PanelTaskList/index.tsx
+++ src/components/PanelTaskList/index.tsx
@@ -1,27 +1,16 @@
 import { useWindowsStore } from "../../store/windowsStore";
 import { APPS } from "../../apps";
+import PanelTaskItem from "../PanelTaskItem";
 
 function PanelTaskList() {
   const activeWindows = useWindowsStore((state) => state.activeWindows)
     .map((activeWindow) => APPS.find((app) => app.id === activeWindow.id))
     .filter((app) => app !== undefined);
-  const toggleMinimize = useWindowsStore((state) => state.toggleMinimize);
 
   return (
     <div className="flex flex-row gap-4">
       {activeWindows.map((activeWindow) => (
-        <div
-          key={activeWindow.id}
-          className="flex flex-row gap-2 items-center cursor-pointer"
-          onClick={() => toggleMinimize(activeWindow.id)}
-        >
-          <img
-            src={activeWindow.iconSrc}
-            alt={activeWindow.windowName}
-            className="h-4 w-4 self-center"
-          />
-          <p className="text-white text-sm">{activeWindow.windowName}</p>
-        </div>
+        <PanelTaskItem activeWindow={activeWindow} key={activeWindow.id} />
       ))}
     </div>
   );
