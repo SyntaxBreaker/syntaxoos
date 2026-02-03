dc2a6d4c6826489e322e69db3bef0a0306e80044
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-02-03 15:34:27 +0100

    Implement SettingsWallpapers component

---
+++ src/components/SettingsWallpapers/index.tsx
@@ -0,0 +1,25 @@
+import { WALLPAPERS } from "../../constants";
+import { useSettingsStore } from "../../store/settingsStore";
+
+function SettingsWallpapers() {
+  const currentWallpaper = useSettingsStore((state) => state.currentWallpaper);
+  const setWallpaper = useSettingsStore((state) => state.setWallpaper);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h3 className="text-white text-xs font-extrabold">Wallpaper</h3>
+      <div className="flex flex-row gap-2 flex-wrap">
+        {WALLPAPERS.map((wallpaper) => (
+          <img
+            key={wallpaper.id}
+            src={wallpaper.src}
+            className={`h-32 w-32 cursor-pointer border-2 ${currentWallpaper === wallpaper.src ? "border-gray-500" : "border-transparent"}`}
+            onClick={() => setWallpaper(wallpaper.src)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SettingsWallpapers;
