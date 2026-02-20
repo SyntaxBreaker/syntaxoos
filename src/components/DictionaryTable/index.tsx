6d2cd5424963e5edde6a3230f9d15c8f7673f5de
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-02-20 17:28:28 +0100

    Implement Table in DictionaryTable

--- src/components/DictionaryTable/index.tsx
+++ src/components/DictionaryTable/index.tsx
@@ -1,3 +1,11 @@
+import TableBadge from "../TableBadge";
+import TableBody from "../TableBody";
+import TableCell from "../TableCell";
+import TableContainer from "../TableContainer";
+import TableHeader from "../TableHeader";
+import TableHeaderCell from "../TableHeaderCell";
+import TableRow from "../TableRow";
+
 interface DictionaryTableProps {
   definitions: string[];
   synonyms: string[];
@@ -5,52 +13,39 @@ interface DictionaryTableProps {
 
 function DictionaryTable({ definitions, synonyms }: DictionaryTableProps) {
   return (
-    <div className="w-full overflow-x-auto rounded-lg border border-gray-800 shadow-sm">
-      <table className="w-full text-left border-collapse bg-gray-800">
-        <thead className="bg-gray-600 border-b border-gray-800">
-          <tr>
-            <th className="px-6 py-4 font-semibold text-gray-200 w-2/3">
-              Definitions
-            </th>
-            <th className="px-6 py-4 font-semibold text-gray-200 w-1/3">
-              Synonyms
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-600 bg-gray-800">
-          {definitions.map((defintion, index) => (
-            <tr key={index}>
-              <td className="px-6 py-4 text-gray-200 leading-relaxed">
-                {defintion}
-              </td>
-              {index === 0 && (
-                <td
-                  rowSpan={definitions.length}
-                  className="px-6 py-4 align-top border-l border-gray-600"
-                >
-                  <div className="flex flex-wrap gap-2">
-                    {synonyms.length > 0 ? (
-                      synonyms.map((synonym) => (
-                        <span
-                          key={synonym}
-                          className="px-2 py-1 bg-gray-600 text-gray-200 rounded text-xs font-medium"
-                        >
-                          {synonym}
-                        </span>
-                      ))
-                    ) : (
-                      <span className="text-gray-200 italic text-sm">
-                        No synonyms
-                      </span>
-                    )}
-                  </div>
-                </td>
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <TableContainer>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell classes="w-2/3 border-r">
+            Definitions
+          </TableHeaderCell>
+          <TableHeaderCell classes="w-1/3">Synonyms</TableHeaderCell>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {definitions.map((definition, index) => (
+          <TableRow key={index}>
+            <TableCell>{definition}</TableCell>
+            {index === 0 && (
+              <TableCell
+                rowSpan={definition.length}
+                classes="align-top border-l border-gray-400"
+              >
+                <div className="flex flex-wrap gap-2">
+                  {synonyms.length > 0 ? (
+                    synonyms.map((synonym, index) => (
+                      <TableBadge key={index} label={synonym} />
+                    ))
+                  ) : (
+                    <span className="text-gray-400 italic text-sm"></span>
+                  )}
+                </div>
+              </TableCell>
+            )}
+          </TableRow>
+        ))}
+      </TableBody>
+    </TableContainer>
   );
 }
 
