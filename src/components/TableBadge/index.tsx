b977c5ca626c944cbda74cf128de55b21e6e90d0
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-02-23 16:25:31 +0100

    Refactor TableBadge component

--- src/components/TableBadge/index.tsx
+++ src/components/TableBadge/index.tsx
@@ -1,10 +1,13 @@
 interface TableBadgeProps {
   label: string;
+  classes?: string;
 }
 
-function TableBadge({ label }: TableBadgeProps) {
+function TableBadge({ label, classes = "" }: TableBadgeProps) {
   return (
-    <span className="px-2 py-1 bg-gray-600 text-gray-200 rounded text-xs font-medium">
+    <span
+      className={`px-2 py-1 bg-gray-600 text-gray-200 rounded text-xs font-medium ${classes}`}
+    >
       {label}
     </span>
   );
