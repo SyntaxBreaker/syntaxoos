60b619baeb86b1e641fd6c2abdf10da3f3f7d0f1
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-03-04 14:32:23 +0100

    Refactor TextReader

--- src/components/TextReader/index.tsx
+++ src/components/TextReader/index.tsx
@@ -5,18 +5,16 @@ function TextReader() {
   const content = useTextStore((state) => state.content);
   const fileFormat = useTextStore((state) => state.fileFormat);
 
-  if (fileFormat === "md") {
-    return (
-      <div className="text-gray-200 py-2">
-        <Markdown>{content}</Markdown>
-      </div>
-    );
-  }
-
   return (
-    <div className="py-2">
-      <p className="text-gray-200 text-sm leading-6">{content}</p>
-    </div>
+    <article className="py-2">
+      {fileFormat === "md" ? (
+        <div className="text-gray-200 py-2">
+          <Markdown>{content}</Markdown>
+        </div>
+      ) : (
+        <pre className="text-gray-200 text-sm leading-6">{content}</pre>
+      )}
+    </article>
   );
 }
 
