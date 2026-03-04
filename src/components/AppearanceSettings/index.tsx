29c0ff9d1f602fcdd9e6b220166617a1e172df4f
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-03-04 12:55:35 +0100

    Implement SettingsBackgroundSize component

--- src/components/AppearanceSettings/index.tsx
+++ src/components/AppearanceSettings/index.tsx
@@ -1,38 +1,11 @@
-import { useSettingsStore } from "../../store/settingsStore";
+import SettingsBackgroundSize from "../SettingsBackgroundSize";
 import SettingsWallpapers from "../SettingsWallpapers";
 
 function AppearanceSettings() {
-  const currentBackgroundSize = useSettingsStore(
-    (state) => state.currentBackgroundSize,
-  );
-  const setBackgroundSize = useSettingsStore(
-    (state) => state.setBackgroundSize,
-  );
-
-  const handleBackgroundSizeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setBackgroundSize(event.target.value);
-  };
-
   return (
     <div className="flex flex-col gap-4">
       <SettingsWallpapers />
-      <div className="flex flex-col gap-2">
-        <label className="text-gray-200 text-xs font-extrabold">
-          Background size
-        </label>
-        <select
-          id="size"
-          className="w-full p-2 bg-transparent border border-gray-600 text-xs rounded-sm text-gray-200"
-          defaultValue={currentBackgroundSize}
-          onChange={handleBackgroundSizeChange}
-        >
-          <option value="Auto">Auto</option>
-          <option value="Cover">Cover</option>
-          <option value="Full">Full</option>
-        </select>
-      </div>
+      <SettingsBackgroundSize />
     </div>
   );
 }
---
+++ src/components/SettingsBackgroundSize/index.tsx
@@ -0,0 +1,36 @@
+import { useSettingsStore } from "../../store/settingsStore";
+
+function SettingsBackgroundSize() {
+  const currentBackgroundSize = useSettingsStore(
+    (state) => state.currentBackgroundSize,
+  );
+  const setBackgroundSize = useSettingsStore(
+    (state) => state.setBackgroundSize,
+  );
+
+  const handleBackgroundSizeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setBackgroundSize(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="text-gray-200 text-xs font-extrabold">
+        Background size
+      </label>
+      <select
+        id="size"
+        className="w-full p-2 bg-transparent border border-gray-600 text-xs rounded-sm text-gray-200"
+        defaultValue={currentBackgroundSize}
+        onChange={handleBackgroundSizeChange}
+      >
+        <option value="Auto">Auto</option>
+        <option value="Cover">Cover</option>
+        <option value="Full">Full</option>
+      </select>
+    </div>
+  );
+}
+
+export default SettingsBackgroundSize;
