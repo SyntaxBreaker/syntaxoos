c80f4ddadf08643a104e5f715d422cb47e4445f4
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-04-10 15:15:53 +0200

    Implement player progress bar in BufferHellHeader

--- src/components/BufferHellHeader/index.tsx
+++ src/components/BufferHellHeader/index.tsx
@@ -11,12 +11,26 @@ function BufferHellHeader({ score }: BufferHellHeaderProps) {
   );
   const playerHP = useBufferHellStore((state) => state.playerHP);
   const playerLevel = useBufferHellStore((state) => state.playerLevel);
+  const experienceToNextLevel = useBufferHellStore(
+    (state) => state.experienceToNextLevel,
+  );
+
+  const playerExperiencePercentage = Math.min(
+    100,
+    (playerExperience / experienceToNextLevel) * 100,
+  );
 
   return (
-    <header className="p-2 bg-gray-800 flex justify-between rounded-lg text-xs text-gray-200 font-mono">
+    <header className="p-2 bg-gray-800 flex justify-between items-center rounded-lg text-xs text-gray-200 font-mono">
       <span>
         HP: {playerHP} | Experience: {playerExperience} | Level: {playerLevel}
       </span>
+      <div className="w-3xs h-2 bg-gray-600 rounded-full overflow-hidden">
+        <div
+          className="h-full bg-emerald-600"
+          style={{ width: `${playerExperiencePercentage}%` }}
+        />
+      </div>
       <span>
         Score: {score} | High Score: {highScore}
       </span>
